039ab7540f591c16efff452eae396142039dd882
Dinangelo <dravalijaona@gmail.com>
2024-06-03 10:45:37 +0300

    Quelques ajustement sur l'affichage des chiffres

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const AFFICHE = document.getElementById('contaffichage');
-const RESULT = document.getElementById('contresultat');
+//const AFFICHE = document.getElementById('contaffichage');
+const AFFICHE = document.getElementById('afficheCont');
+//const RESULT = document.getElementById('contresultat');
+const RESULT = document.getElementById('afficheRes');
 const BUTTONS = document.querySelectorAll('button');
 BUTTONS.forEach(button => {
     button.addEventListener('click', () => {
@@ -7,17 +9,25 @@ BUTTONS.forEach(button => {
         if (button.innerText === '=') {
             try {
                 console.log(RESULT.innerHTML);
-                RESULT.innerHTML = eval(AFFICHE.innerHTML);
-                
+                RESULT.value = eval(AFFICHE.value);
 
             } catch (error) {
-                AFFICHE.value = 'ERREUR';
+                RESULT.value = 'ERREUR';
             }
         } else if (button.innerText === 'C') {
-            AFFICHE.innerHTML = '';
+            AFFICHE.value = "";
 
+        } else if (button.innerHTML === 'Ret') {
+            effacerDernier();
         } else {
-            AFFICHE.innerHTML += button.innerText; 
+            console.log(AFFICHE.value);
+            AFFICHE.value += button.innerText; 
         }
     });
 });
+
+function effacerDernier() {
+    var aff = AFFICHE.value;
+    var newAff = aff.substring(0, aff.length - 1);
+    document.getElementById('afficheCont').value = newAff;
+}
